05ed0f593d5992b095b681708a3272edc423c97f
dan0102dan <me@dan0102dan.ru>
2021-01-13 18:16:38 +0300

    Updates

---
+++ precache-manifest.49a0604c45bc176b3b6bfb1deec597f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971e14ba0a3c6682a3532f3746048585",
+    "url": "/dan0102dan.github.io/index.html"
+  },
+  {
+    "revision": "0c4d5eb2e24fbd19cfa2",
+    "url": "/dan0102dan.github.io/static/css/2.eff834ed.chunk.css"
+  },
+  {
+    "revision": "9afb24ad9062b76d66be",
+    "url": "/dan0102dan.github.io/static/css/main.0b2a7f78.chunk.css"
+  },
+  {
+    "revision": "0c4d5eb2e24fbd19cfa2",
+    "url": "/dan0102dan.github.io/static/js/2.0c185654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dan0102dan.github.io/static/js/2.0c185654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9afb24ad9062b76d66be",
+    "url": "/dan0102dan.github.io/static/js/main.6c5bcd2c.chunk.js"
+  },
+  {
+    "revision": "3c88fb86f546cc429a20",
+    "url": "/dan0102dan.github.io/static/js/runtime-main.b628d371.js"
+  },
+  {
+    "revision": "2617040eead9665362a6df661b9dc59a",
+    "url": "/dan0102dan.github.io/static/media/2spotify.2617040e.png"
+  },
+  {
+    "revision": "d549df088e53fea4eb24a8f66dbdc30b",
+    "url": "/dan0102dan.github.io/static/media/converter.d549df08.png"
+  },
+  {
+    "revision": "a408f244c336f077b799f0a8d20069f3",
+    "url": "/dan0102dan.github.io/static/media/newRelease.a408f244.png"
+  }
+]);
